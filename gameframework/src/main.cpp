8b199315b1f0ef6bfd70852ff38bd887927ee04b
Léo Küttel <leo.kuettel@divtec.ch>
2022-01-24 08:22:22 +0100

    Mise à jour du code

--- gameframework/src/main.cpp
+++ gameframework/src/main.cpp
@@ -5,22 +5,18 @@
  * \section Préambule
  * Dans le cadre de l’atelier de 31-Programmation OO enseigné par Jérôme Connus,
  * nous devons réaliser un jeu vidéeo pour testé nos connaissance en c++. Le code sera par la suite évaluée.
- *
  * \section intro_sec Introduction
  * Amongus-Jumper est un jeu vidéo dit Platformer. Un Platformer est un sous-genre du jeux vidéo d’action qui consiste à contrôler un personnage
  * qui doit sauter sur des plateformes dans les airs et éviter des obstacles. vous pouvez retrouvé le code du joueur dans \ref Character ainsi que dans
  * le \ref GameCore.
- *
  * \section Entité
  * les Sprites ayant une vélocité, pouvant être détruit ou étant soumis à la gravité sont considérés comme vivants, et sont donc des entités.
  * Actuellement, il existe trois classes qui découlent de la classe \ref Entity.
  * Les classes \ref Bulio, \ref Character et \ref CaisseAmovible gérant respectivement les ennemis, le joueur et les caisses en bois.
- *
- * \section1 Gestion des collisions
+ * \section Gestion des collisions
  * Les collisions sont détectées et utilisées par les entités du jeu. La plupart des entités comme les caisses gèrent leur collision par leur TickHandler()
  * (qui est activé dans le tick() du \ref gamecore).
  * Le TickHandler() va s’occuper des collisions (selon le type d’entité) par anticipation ou par collision actuelle.
- *
  * \subsection Collision par anticipation
  * La collision par anticipation est utilisée essentiellement pour la gestion de la gravité.
  * Effectivement, on en a besoin pour savoir à l’avance à quel moment l’entité devra être attirée par le bas ou non.
@@ -34,7 +30,6 @@
  * Pour ce faire, les entités utilisent la méthode getCollisionLocate() qui récupère les valeurs de la fonction intersected()
  * mise à dispositions par Qt. Celle-ci renvoie l'intersection entre les deux rectangles donnés.
  * En l’occurrence, on utilise la bouding Box des deux sprites pour ensuite localiser la zone en contact.
- *
  * \section Annexes
  *  Vous retrouvez plus d'informations dans les 3 PDF dans répertoire doc.
  */
